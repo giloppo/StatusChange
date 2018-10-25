4b1cd24246534e3b87738d550cd5897587045dcc
giloppo <giloppo7@gmail.com>
2018-10-25 20:16:55 +0900

    Ver 1.0

--- StatusChange/Program.cs
+++ StatusChange/Program.cs
@@ -6,11 +6,75 @@ using System.Threading.Tasks;
 
 namespace StatusChange
 {
-    class Program
+    public class MouseStatus
+    {
+        public int x_down { get; set; }
+        public int y_down { get; set; }
+        public bool isDown { get; set; }
+    }
+
+    public class PlayerStatus
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int velocity { get; set; }
+        public int direction { get; set; }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            var player = new PlayerStatus
+            {
+                x = 0,
+                y = 0,
+                velocity = 0
+            };
+            var mouse = new MouseStatus
+            {
+                x_down = 10,
+                y_down = 10,
+                isDown = true
+            };
+
+            CallThisEveryFrame(mouse, player);
+        }
+
+        public static void CallThisEveryFrame(MouseStatus mouse, PlayerStatus player)
+        {
+            if (mouse.isDown)
+            {
+                MovePlayer(mouse, player);
+            }
+            else
+            {
+                StopPlayer(mouse, player);
+            }
+        }
+
+        public static void MovePlayer(MouseStatus mouse, PlayerStatus player)
+        {
+            if (mouse.x_down != player.x || mouse.y_down != player.y)
+            {
+                player.velocity = 10;
+                player.direction = SetPlayerDirection();
+            }
+        }
+
+        public static void StopPlayer(MouseStatus mouse, PlayerStatus player)
+        {
+            if (mouse.x_down == player.x && mouse.y_down == player.y && player.velocity != 0)
+            {
+                player.velocity = 0;
+                player.direction = SetPlayerDirection();
+            }
+        }
+
+        public static int SetPlayerDirection()
+        {
+            //To Do
+            return 0;
         }
     }
 }
